9bc412fbb13efe64898a2f42a1d34fff046c39a4
seanjahaupan <seanjahaupan@gmail.com>
2017-06-01 17:39:43 -0700

    validperfectsquare

---
+++ ValidPerfectSquare.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        i = 1
+        while True:
+            newSquare = i * i
+            if newSquare == num:
+                return True
+            if newSquare > num:
+                return False
+            i += 1
+            
