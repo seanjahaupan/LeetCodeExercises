f312982a553c6f5c5e282c88d0f1d6d0118055f6
seanjahaupan <seanjahaupan@gmail.com>
2017-06-03 10:55:29 -0700

    tallest candle

---
+++ tallestCandle.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+height = [int(height_temp) for height_temp in input().strip().split(' ')]
+
+tallestCandle = 0
+numberOfCandles = 0
+
+for candle in height:
+    if candle > tallestCandle:
+        tallestCandle = candle
+        numberOfCandles = 1
+    elif candle == tallestCandle:
+        numberOfCandles += 1
+        
+        
+print (numberOfCandles)  
