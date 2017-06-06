b9213399f9f5ef8c56e955f6db73fc3d15d42f8f
seanjahaupan <seanjahaupan@gmail.com>
2017-06-05 18:09:16 -0700

    plus minus

---
+++ plusminus.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+positive = 0
+negative = 0
+zeros = 0
+for number in arr:
+    if number == 0:
+        zeros += 1
+    elif number > 0:
+        positive += 1
+    elif number < 0:
+        negative += 1
+        
+        
+print(round(positive / len(arr), 6))
+print(round(negative / len(arr), 6))
+print(round(zeros / len(arr), 6))
