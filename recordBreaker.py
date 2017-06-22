96cceb840fb230b19b4e6e8a78cb8d1f3ee77891
seanjahaupan <seanjahaupan@gmail.com>
2017-06-21 17:35:55 -0700

    record breaker

---
+++ recordBreaker.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import sys
+
+def getRecord(s):
+    # Complete this function
+    highest = s[0]
+    lowest = s[0]
+    highestCount = 0
+    lowestCount = 0
+    
+    for game in s:
+        if game > highest:
+            highest = game
+            highestCount += 1
+        elif game < lowest:
+            lowest = game
+            lowestCount += 1
+    return [highestCount,lowestCount]
+
+n = int(input().strip())
+s = list(map(int, input().strip().split(' ')))
+result = getRecord(s)
+print (" ".join(map(str, result)))
