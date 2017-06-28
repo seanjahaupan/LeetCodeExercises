2662ba91928ec920b2d468319f224f65f81ddfb5
seanjahaupan <seanjahaupan@gmail.com>
2017-06-28 16:20:50 -0700

    bon appetit

---
+++ BonAppetit.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import sys
+
+def bonAppetit(n, k, b, ar):
+    # Complete this function
+    shouldCharge = 0
+    for i in range(n):
+        if i != k:
+            shouldCharge += ar[i]
+            
+    shouldCharge /= 2
+    if shouldCharge - b == 0:
+        return 'Bon Appetit'
+    else:
+        return int(b - shouldCharge)
+
+n, k = input().strip().split(' ')
+n, k = [int(n), int(k)]
+ar = list(map(int, input().strip().split(' ')))
+b = int(input().strip())
+result = bonAppetit(n, k, b, ar)
+print(result)
