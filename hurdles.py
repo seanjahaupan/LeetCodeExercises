c37d9ac3ca3e81636e9ca31567a670922bc21d7a
seanjahaupan <seanjahaupan@gmail.com>
2017-07-05 17:38:04 -0700

    hurdles

---
+++ hurdles.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import sys
+
+
+n,k = input().strip().split(' ')
+n,k = [int(n),int(k)]
+height = list(map(int, input().strip().split(' ')))
+# your code goes here
+
+print(max(0,max(height) - k))
